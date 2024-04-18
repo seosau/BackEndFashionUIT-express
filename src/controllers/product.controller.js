38223558e0037847a21e0c8d40e1778e76f3fba3
Devbeee <tngb1002@gmail.com>
2024-04-18 20:39:41 +0700

    Add: api edit product

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -8,10 +8,12 @@ class ProductController {
     try {
       const products = await Product.find({});
       const updatedProducts = products.map((product) => {
-        const updatedImages = product.images.map(image => ({
+        const updatedImages = product.images.map((image) => ({
           color: image.color,
-          imgUrl: `http://localhost:8000/uploads/${path.basename(image.imgUrl)}`
-      }));
+          imgUrl: `http://localhost:8000/uploads/${path.basename(
+            image.imgUrl
+          )}`,
+        }));
         return {
           ...product.toObject(),
           images: updatedImages,
@@ -55,14 +57,66 @@ class ProductController {
       .save()
       .then(() => {
         console.log("Tạo sản phẩm thành công");
-        res.status(201).json({ error: "Tạo sản phẩm thành công" });
+        res.status(201).json({ success: "Tạo sản phẩm thành công" });
       })
       .catch((err) => {
         console.error("Lỗi khi tạo sản phẩm:", err);
         res.status(500).json({ error: "Đã xảy ra lỗi khi tạo sản phẩm" });
       });
   }
-  update(req, res, next) {}
+  async getproductbyslug(req, res, next) {
+    try {
+      const { slug } = req.params;
+      const product = await Product.findOne({ slug: slug });
+      product.images = product.images.map((image) => ({
+        color: image.color,
+        imgUrl: `http://localhost:8000/uploads/${path.basename(image.imgUrl)}`,
+      }));
+      res.status(200).json(product);
+    } catch (error) {
+      res.status(500).json({ error: "Đã xảy ra lỗi khi tìm sản phẩm" });
+    }
+  }
+  async update(req, res, next) {
+    const controllersDirectory = path.join(__dirname, "..");
+    const uploadDirectory = path.join(controllersDirectory, "uploads");
+    if (!fs.existsSync(uploadDirectory)) {
+      fs.mkdirSync(uploadDirectory);
+    }
+    function saveImageBase64ToDisk(base64String, fileName) {
+      const base64Data = base64String.replace(/^data:image\/\w+;base64,/, "");
+      const imageBuffer = Buffer.from(base64Data, "base64");
+      const imagePath = path.join(uploadDirectory, fileName);
+      fs.writeFileSync(imagePath, imageBuffer);
+      return imagePath;
+    }
+    const { slug } = req.params;
+    const editedProduct = { ...req.body, slug: convertToSlug(req.body.name) };
+    const preProduct = {
+      ...editedProduct,
+      images: [
+        ...editedProduct.images.map((image, index) => {
+          if (!!image.imgUrl.match(/^data:image\/\w+;base64,/))
+            return {
+              color: image.color,
+              imgUrl: saveImageBase64ToDisk(
+                image.imgUrl,
+                `${editedProduct.slug}-${index}.jpg`
+              ),
+            };
+          return image;
+        }),
+      ],
+    };
+    Product.updateOne({ slug: slug }, preProduct)
+      .then((response) => {
+        res.status(200).json({ message: "Cập nhật sản phẩm thành công" });
+      })
+      .catch((error) => {
+        res.status(500).json({ message: "Cập nhật sản phẩm thất bại", error });
+      });
+  }
+
   delete(req, res, next) {}
 }
 
--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const ProductController = require("../controllers/product.controller");
 
-router.post('/product/store', ProductController.store);
-router.put("/product/update/:id", ProductController.update);
-router.delete("/product/delete/:id", ProductController.delete);
+
+router.post("/product/store", ProductController.store);
+router.get("/product/:slug", ProductController.getproductbyslug);
+router.put("/product/update/:slug", ProductController.update);
+router.delete("/product/delete/:slug", ProductController.delete);
 router.get("/products", ProductController.index);
 
 module.exports = router;
