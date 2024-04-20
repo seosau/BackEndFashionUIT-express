0f11da4c8252fd151f55ad52a4bce3e1bc351717
Devbeee <tngb1002@gmail.com>
2024-04-21 00:57:51 +0700

    Add: api store contact info

--- src/routes/site.route.js
+++ src/routes/site.route.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const locationRouter = require('./location.route')
-
-router.use('/location', locationRouter);
-
+const locationRouter = require("./location.route");
+const contactInfoRouter = require("./contact.route");
+router.use("/location", locationRouter);
+router.use("/contact", contactInfoRouter);
 module.exports = router;
